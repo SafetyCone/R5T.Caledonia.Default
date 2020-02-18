b590371842360550c04e996b33e3505567eb066d
MinexAutomation <minex.automation@gmail.com>
2020-02-17 18:25:41 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Caledonia.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Caledonia.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="DefaultCommandLineInvocationOperator"/> implementation of <see cref="ICommandLineInvocationOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultCommandLineInvocationOperator(this IServiceCollection services)
+        {
+            services.AddSingleton<ICommandLineInvocationOperator, DefaultCommandLineInvocationOperator>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DefaultCommandLineInvocationOperator"/> implementation of <see cref="ICommandLineInvocationOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ICommandLineInvocationOperator> AddDefaultCommandLineInvocationOperatorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ICommandLineInvocationOperator>(() => services.AddDefaultCommandLineInvocationOperator());
+            return serviceAction;
+        }
+    }
+}
