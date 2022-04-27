41a128c68a8d075ded19bb847262f665bbb5b63a
davidcoats <david@getsafetycone.com>
2022-04-27 15:25:54 -0700

    Interim changes.

---
+++ source/R5T.Caledonia.Default/Code/Services/Implementations/DefaultCommandLineInvocationOperator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+using R5T.T0064;
+
+
+namespace R5T.Caledonia.Default
+{
+    [ServiceImplementationMarker]
+    public class DefaultCommandLineInvocationOperator : ICommandLineInvocationOperator, IServiceImplementation
+    {
+        public int Run(string command, string arguments, DataReceivedEventHandler receiveOutputData, DataReceivedEventHandler receiveErrorData)
+        {
+            var exitCode = CommandLineInvocationRunner.Run(command, arguments, receiveOutputData, receiveErrorData);
+            return exitCode;
+        }
+
+        public CommandLineInvocationResult Run(CommandLineInvocation invocation)
+        {
+            var result = CommandLineInvocationRunner.Run(invocation);
+            return result;
+        }
+    }
+}
